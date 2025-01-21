c679d04c18ec41c677a4711e2460e57a6f57873c
EMMMABK <adilbikiev2@gmail.com>
2025-01-21 15:29:50 +0600

    add Func

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import './App.css';
+import Currency from './components/Currency/Currency';
 
 function App() {
   return (
+   <> 
     <h1>Currency Converter</h1>
+    <Currency />
+    <div>=</div>
+    <Currency/>
+  </>
   );
 }
 
---
+++ src/components/Currency/Currency.jsx
@@ -0,0 +1,13 @@
+ import React from 'react'
+ 
+ export default function Currency() {
+   return (
+     <div>
+       <input type="number" />
+       <select name="currency_option" id="">
+          <option value="USD">USD</option>
+       </select>
+     </div>
+   )
+ }
+ 
