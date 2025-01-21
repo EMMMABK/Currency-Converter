89a74e9bf5a2978eabf96d7ae5ab643a644f0d33
EMMMABK <adilbikiev2@gmail.com>
2025-01-21 15:26:06 +0600

    add New File and fixed some bugs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
+import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// This is the ID of the div in your index.html file
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+);
