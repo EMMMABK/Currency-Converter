1c6237997d236763e7c4c8d4e54f56f02d9e98f1
EMMMABK <adilbikiev2@gmail.com>
2025-01-21 16:14:35 +0600

    add CurrencyInConsole

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import './App.css';
 import Currency from './components/Currency/Currency';
 
-const BASE_URL = BASE_URL
+BASE_URL = BASE_URL
 
 function App() {
+  const [currencyOptions, setCurrencyOptions] = useState([])
+
   useEffect(() => {
     fetch(BASE_URL)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => setCurrencyOptions([data.base, ...Object.keys(data.rates)]))
   })
   return (
    <> 
